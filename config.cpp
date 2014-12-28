c03d2db6ce9511abbde6b71a950027de92aab7ee
toadball <asmsandrew@googlemail.com>
2014-12-28 18:09:44 +0000

    m

--- config.cpp
+++ config.cpp
@@ -1,67 +1,36 @@
-#include "\ca\weapons_E\basicdefines.hpp"
-class CfgPatches
-{
-	class CAWeapons_E_M240
-	{
+class CfgPatches {
+	class tb_lmg_mag58 {
 		units[] = {};
-		weapons[] = {m240_scoped_E};
-		requiredVersion = 1.02;
-		requiredAddons[] = {"CAWeapons_E","CAweapons"};
+		weapons[] = {tb_lmg_mk25};
+		requiredVersion = 1;
+		requiredAddons[] = {"A3_Weapons_F_beta"};
 	};
 };
-class CfgWeapons
-{
-	class m240;
-	class m240_scoped_EP1: m240
-	{
-    htMin = 1;
-    htMax = 600;
-    afMax = 0;
-    mfMax = 0;
-    mFact = 1;
-    tBody = 100;
-	
-		scope = 2;
-		displayName = $STR_EP1_DN_m240_scoped_EP1;
-		picture="\ca\weapons_E\Data\icons\m240_scoped_CA.paa";
-		modelOptics = "\Ca\weapons_E\M249\M145.p3d";
-		visionMode[] = {"Normal"};
-		opticsPPEffects[]={"OpticsCHAbera2","OpticsBlur2"};
-		opticsZoomMax = 0.0732; distanceZoomMax=300;
-		opticsZoomMin = 0.0732; distanceZoomMin=300;
-		opticsZoomInit= 0.0732;
-		model="\ca\weapons_E\M240\m240_scoped";
-		class Library {libTextDesc = $STR_EP1_LIB_m240_scoped_EP1;}; // TODO
-		class OpticsModes
-		{
-			class M145
-			{
-				opticsID = 1;
-				useModelOptics = true;
-				opticsPPEffects[]={"OpticsCHAbera1","OpticsBlur1"};
-				opticsZoomMax = 0.0732; distanceZoomMax=300;
-				opticsZoomMin = 0.0732; distanceZoomMin=300;
-				opticsZoomInit= 0.0732;
-				memoryPointCamera = "opticView";
-				visionMode[] = {"Normal"};
-				opticsFlare = true;
-				opticsDisablePeripherialVision = true;
-				cameraDir = "";
-			};
-			/*
-			class Ironsigts:M145
-			{
-				opticsID = 2;
-				useModelOptics = false;
-				opticsFlare = false;
-				opticsDisablePeripherialVision = false;
-				opticsZoomMin=0.25;
-				opticsZoomMax=1.1;
-				opticsZoomInit=0.5;
-				memoryPointCamera = "eye";
-				visionMode[] = {};
-			};
-			*/
+
+class CfgWeapons {
+
+	class Rifle_Long_Base_F;
+	class LMG_Zafir_F: Rifle_Long_Base_F { 
+		class FullAuto;
+		class close;
+		class short;
+		class medium;
+		class far_optic1;
+		class far_optic2;
+		
+		
+		class WeaponSlotsInfo {
+			class MuzzleSlot;
+			class CowsSlot;
+			class PointerSlot;
 		};
+	};	
+	class tb_lmg_mk25 : LMG_Zafir_F {
+		tmr_autorest_deployable = 1;
+		displayName = "Mk 25 GPMG";
+		picture = "\tb_lmg_mag58\data\w_mk25_ca.paa";
+		model = "\tb_lmg_mag58\m240_belt";
+		modes[] = {"FullAuto", "close", "short", "medium", "far_optic1", "far_optic2"};
+		handanim[] = {};
 	};
 };
